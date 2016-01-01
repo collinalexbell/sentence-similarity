7004b92afce92b4d41cc46b1c2991cb873284e92
slightlycyborg <taggartmachines@gmail.com>
2016-01-01 05:58:03 -0500

    did a lot of work in nyc. I almost have the depth working. The distance works

--- src/semantic_similarity/core.clj
+++ src/semantic_similarity/core.clj
@@ -6,6 +6,10 @@
                \A,\B,\C,\D,\E,\F,\G,\H,\I,\J,\K,\L,\M,\N,\O,\P,\Q,\R,\S,\T,\U,\V,\W,\X,\Y,\Z})
 
 
+(defn print-n-return [thing]
+  (println thing)
+  thing)
+
 (defn get-only-alpha [my-str]
   (apply str (filter #(letters %) my-str)))
 
@@ -32,22 +36,82 @@
       (/ (- indent-count 3) 4)))) 
  
 
-(defn tree-to-level-map [word-trees]
-    (flatten (map 
-      #(map 
-        (fn [synonyms] 
-          (let [level (determine-levels synonyms)] 
-          (map
-            (fn [split-synonym] 
-              (hash-map 
-                :word (get-only-alpha split-synonym)
-                :level level)) 
-            (split synonyms #","))))
-            (rest (split % #"\n")))
-    ;discard the first part
-    (rest
-      ;break up the word trees by Sense 
-      (apply concat (map #(split %1 #"Sense") word-trees)))))) 
+(defn handle-a-level [state level-string]
+  (let [level-num (determine-levels level-string) id (+ (state :id) 1)] 
+  {
+   :id
+   id
+
+   :cur-path
+   (if (> (count (state :cur-path)) level-num)
+     (conj (subvec (state :cur-path) 0 level-num) id) 
+     (conj (state :cur-path) id))
+
+   :sense 
+   (conj (state :sense)
+    {:id id 
+     :parent (last (butlast (state :cur-path)))
+     :data (map ;;this will return a level broken into a vector of synonym maps
+      (fn [split-synonym] 
+        (hash-map 
+          :word (get-only-alpha split-synonym)
+          :level level-num)) 
+      (split level-string #","))})}))
+
+
+(defn split-str-into-senses [word-tree-string]
+  (rest (apply concat (map #(split %1 #"Sense") word-tree-string))))
+
+(defn children-from-parent [sense]
+  (reduce 
+    (fn [child-struct level] (if (level :parent) 
+                               (assoc child-struct 
+                                    (level :parent) 
+                                    (conj (child-struct (level :parent)) (level :id))) 
+                               child-struct ))
+     (into (sorted-map) (do (map (fn [level] [(level :id)[]])
+      sense))) 
+    sense))
+
+(defn depth-from-children-struct [children-struct]
+  (loop [depth-struct (sorted-map)
+         index (- (count children-struct) 1)]
+    ;(println index)
+    (let [new-depth-struct
+          (if (> (count (children-struct index)) 0)
+            (assoc depth-struct index (+ 1 (reduce 
+                                            #(if 
+                                                (> (depth-struct %1) (depth-struct %2))
+                                                (depth-struct %1)
+                                                (depth-struct %2))
+                                              (children-struct index))))
+            (assoc depth-struct index 1))]
+      ;(println new-depth-struct)
+      (if (= 0 index)
+        new-depth-struct  ;terminating condition 
+        (recur new-depth-struct (- index 1))))))
+
+(defn insert-depths [strange-data-structure]
+  (let [sense (strange-data-structure :sense)]
+    (println "Depth-from-children-struct")
+    (println (depth-from-children-struct
+                (print-n-return (children-from-parent sense))))
+    strange-data-structure
+    ))
+
+(defn handle-a-sense [sense]
+  (let [split-levels (rest (split sense #"\n"))] 
+    (->> split-levels
+        (reduce handle-a-level 
+                {:id -1 :cur-path[] :sense []})
+        (insert-depths) 
+        )))
+
+(defn tree-to-level-map [word-tree-string]
+  (flatten 
+    (map 
+      handle-a-sense 
+      (split-str-into-senses word-tree-string)))) 
 
 (defn collect-duplicates [word-tree]
   (into {} (filter #(if (= "" (first %1)) false true)
@@ -88,6 +152,12 @@
     (collect-duplicates))] 
 
     (get-common-ancestors tree1 tree2))) 
+
+(defn test-handle-a-sense []
+       (-> "dog"
+          (get-word-trees)
+          (tree-to-level-map)))
+       
 
 
 
